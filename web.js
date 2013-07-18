4477fd68f4728ca16f7dc6b5791c3dd91e240538
Duc Ha <duchatran@gmail.com>
2013-07-18 05:26:11 +0000

    Added code to read index.hml

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var buff = fs.readFileSync('index.html');
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buff.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
